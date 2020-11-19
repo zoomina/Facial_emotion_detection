9bf383eb643df17653187f3791111d57817b6b0c
zoomina <seolhwa8191@gmail.com>
2020-11-19 17:39:30 +0900

    edit train
    >> to save train summary, add plot functions

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 from torch.optim.lr_scheduler import StepLR
-from torchvision.transforms import ToTensor
-import tqdm
+import matplotlib.pyplot as plt
 import os
 
 from model.emo_vgg import *
@@ -14,10 +13,12 @@ def calc_accuracy(X,Y):
 def train(train_dataloader, dev_dataloader, model, device, batch_size, optimizer, criterion, epoch):
     max_grad_norm = 1
     log_interval = 200
+    train_summary = {"loss": [], "acc": [], "eval":[]}
     for e in range(epoch):
         train_acc = 0.0
         test_acc = 0.0
         scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+        step_summary = {"loss" : 0, "acc" : 0, "eval" : 0}
         model.train()
         for batch_id, (img, label) in enumerate(train_dataloader):
             optimizer.zero_grad()
@@ -30,9 +31,14 @@ def train(train_dataloader, dev_dataloader, model, device, batch_size, optimizer
             optimizer.step()
             scheduler.step()  # Update learning rate schedule
             train_acc += calc_accuracy(out, label)
+            step_summary["loss"] += loss.data
+            step_summary["acc"] += train_acc / (batch_id + 1)
             if batch_id % log_interval == 0:
                 print("epoch {} batch id {} loss {} train acc {}".format(e + 1, batch_id + 1, loss.data.cpu().numpy(),
                                                                          train_acc / (batch_id + 1)))
+        train_summary["loss"].append(step_summary["loss"] / batch_size)
+        train_summary["acc"].append(step_summary["acc"] / batch_size)
+
         print("epoch {} train acc {}".format(e + 1, train_acc / (batch_id + 1)))
         model.eval()  # 모델 평가 부분
         for batch_id, (img, label) in enumerate(dev_dataloader):
@@ -40,10 +46,20 @@ def train(train_dataloader, dev_dataloader, model, device, batch_size, optimizer
             label = torch.FloatTensor(label).to(device)
             out = model(img)
             test_acc += calc_accuracy(out, label)
+            step_summary["eval"] += test_acc / (batch_id + 1)
+        train_summary["eval"].append(step_summary["eval"] / batch_size)
         print("epoch {} test acc {}".format(e + 1, test_acc / (batch_id + 1)))
+
 
     if not os.path.exists("./result"):
         os.mkdir("./result")
+    plt.subplot(1, 3, 1)
+    plt.plot(range(len(train_summary["loss"])), train_summary["loss"], label="train_loss")
+    plt.subplot(1, 3, 2)
+    plt.plot(range(len(train_summary["acc"])), train_summary["acc"], label="train_acc")
+    plt.subplot(1, 3, 3)
+    plt.plot(range(len(train_summary["eval"])), train_summary["eval"], label="eval_acc")
+    plt.imsave("result/train_summary_epoch{}_batch{}.jpg".format(epoch, batch_size))
     torch.save(model.state_dict(), 'result/epoch{}_batch{}.pt'.format(epoch, batch_size))
 
 if __name__ == "__main__":
